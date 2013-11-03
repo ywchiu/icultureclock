80ad13edba4577b3efd81a715ca9329164bbf1cd
Willy Kuo <waitingkuo0527@gmail.com>
2013-11-03 11:42:24 +0800

    change link

--- chrome-extension/add_link.js
+++ chrome-extension/add_link.js
@@ -3,10 +3,10 @@ $(document).ready(function(){
     $('.masonry-brick').each(function(){
       if ($(this).find('.iclock').length > 0){}
       else {
-        $(this).append('<div class="iclock" style="background-color: transparent; position: relative; z-index: 99999;top: -30px; text-align: right; margin-right: 70px;;"><a class="iclock-link" href="http://localhost:8000/index.html">iClock</a></div>');
+        $(this).append('<div class="iclock" style="background-color: transparent; position: relative; z-index: 99999;top: -30px; text-align: right; margin-right: 70px;;"><a class="iclock-link" href="http://localhost:8000/search.html">iClock</a></div>');
         $('.iclock-link').click(function(){
           console.log('click');
-          window.location = 'http://localhost:8000/index.html';
+          window.location = 'http://localhost:8000/search.html';
         });
       }
     });
